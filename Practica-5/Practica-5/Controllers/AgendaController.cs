703728f4cccf70f381f6f3cf993823b7dd96a3c5
gp <gp@RONALD>
2019-11-20 13:30:27 -0400

    Conituacion de proyecto, Creacion De la consulta de asignatura y nueva conexion de base de datos.

---
+++ Practica-5/Practica-5/Controllers/AgendaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practica_5.Models;
+
+namespace Practica_5.Controllers
+{
+    public class AgendaController : Controller
+    {
+        private ConexionDBTareas db = new ConexionDBTareas();
+
+        // GET: Agenda
+        public ActionResult Index()
+        {
+            return View(db.agenda.ToList());
+        }
+
+        // GET: Agenda/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            agenda agenda = db.agenda.Find(id);
+            if (agenda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agenda);
+        }
+
+        // GET: Agenda/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agenda/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,celular,email,direccion")] agenda agenda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.agenda.Add(agenda);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agenda);
+        }
+
+        // GET: Agenda/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            agenda agenda = db.agenda.Find(id);
+            if (agenda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agenda);
+        }
+
+        // POST: Agenda/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,celular,email,direccion")] agenda agenda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agenda).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agenda);
+        }
+
+        // GET: Agenda/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            agenda agenda = db.agenda.Find(id);
+            if (agenda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agenda);
+        }
+
+        // POST: Agenda/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            agenda agenda = db.agenda.Find(id);
+            db.agenda.Remove(agenda);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Practica-5/Practica-5/Controllers/HomeController.cs
+++ Practica-5/Practica-5/Controllers/HomeController.cs
@@ -3,28 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Practica_5.Models;
+
+using System.Data;
+using System.Data.Entity;
+using System.Net;
 
 namespace Practica_5.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        private ConexionDBTareas db = new ConexionDBTareas();
 
+        // GET: Home
+
+
+        /*Este Action Resulta es para buscar en un vista distina el nombre del Estudiantes y 
+         * luego ingresar la opcion deseada para dirigirlo hacia el ActionResult de COnsulta*/
+        public ActionResult BusquedaConsulta()
+
+        {
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
+        public ActionResult Consultas()
+        {
+            return View(db.Students.ToList());
+        }
+
+        public ActionResult Lector()
+        {
             return View();
         }
+
+        public ActionResult Acercade()
+        {
+            return View();
+        }
+
     }
 }
